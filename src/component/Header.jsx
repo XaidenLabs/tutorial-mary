dd865f7ebae9616072f6e6796644696a3fb735b9
DADIENG ALFRED GABRIEL <dadiengalfred18@gmail.com>
2026-01-18 22:07:49 +0100

    feat: initialize React project with Vite, ESLint, and Tailwind CSS with basic components.

---
+++ src/component/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <nav className='flex justify-between text-blue-500 bg-gray-200 p-2 m-2'>
+        <h4>Home</h4>
+        <h4>About</h4>
+        <h4>Services</h4>
+        <h4>Contact</h4>
+    </nav>
+    </>
+  )
+}
+
+export default Header
